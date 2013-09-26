5d76cefe228d44178f95bec33e2a3c2c6c48e7f1
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-25 21:15:11 -0500

    db migrations, models, established routes and views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,22 +3,64 @@ get '/' do
   erb :index
 end
 
-get '/create_user' do
+get '/signup' do
   # links to create_user page 
 end 
+
+get '/profile' do
+  # @user = User.find(params[:id])
+  # @user_notes = @user.notes
+  erb :profile
+end 
+
+get '/create_note' do
+  erb :create_note
+end
 
 get '/notes' do
   # directs to # full page of user's notes 
 end 
 
+get '/logout' do
+  session.clear
+  redirect '/'
+end
 
 
 # ====== POST =============
 
-post '/create_user' do
+post '/login' do  
+  #collects login information and redirects to home page 
+  @user = User.find_by_username(params[:username])
+  if @user
+    if @user.password = params[:password]
+      session[:id] = @user.id
+      erb :index
+    else
+      @notpass = "Sweetie Daaahling, go home, you're drunk!"
+      redirect '/'
+    end 
+  else 
+      @notpass = "Sweetie Daaahling, go home, you're drunk!"
+      erb :index
+  end
+end 
+
+post '/signup' do
   # saves new user info to db
+
+  if params[:password] == params[:confirm_password]
+    @new_user = User.create(username: params[:username], password: params[:password])
+    session[:id] = @new_user.id
+    erb :index
+  end 
 end
 
 post '/create_note' do
   # creates and saves a new note to the db 
+
+  @post = Note.create(title: params[:title], content: params[:content])
+  session[:id] = @new_user.id
+  erb :index
 end
+
